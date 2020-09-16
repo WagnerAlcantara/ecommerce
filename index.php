f1021ed608c93f888208019646b6fc9968cc859b
Wagner Alcantara <wagner.alcantara@outlook.com.br>
2020-09-16 16:11:51 -0300

    Criando o template de usuário e funçõa list all

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
@@ -17,7 +17,8 @@ $app->get('/', function () {
 });
 
 
-$app->get('/admin', function () {
+$app->get('/admin/', function () {
+	User::verifyLogin();
 	$page = new PageAdmin();
 	$page->setTpl("index");
 });
@@ -33,5 +34,44 @@ $app->post('/admin/login', function () {
 	header("Location: /admin");
 	exit;
 });
+
+$app->get('/admin/logout', function () {
+	User::logout();
+	header("Location: /admin/login");
+	exit;
+});
+
+$app->get('/admin/users', function () {
+	User::verifyLogin();
+	$users = User::listAll();
+	$page = new PageAdmin();
+	$page->setTpl("users", array("users" => $users));
+});
+$app->get('/admin/users/create', function () {
+	User::verifyLogin();
+	$page = new PageAdmin();
+	$page->setTpl("users-create");
+});
+
+$app->get('/admin/users/:iduser/delete', function ($iduser) {
+	User::verifyLogin();
+});
+
+
+
+
+$app->get('/admin/users/:iduser', function ($iduser) {
+	User::verifyLogin();
+	$page = new PageAdmin();
+	$page->setTpl("users-update");
+});
+$app->post('/admin/users/create', function () {
+	User::verifyLogin();
+});
+$app->post('/admin/users/:iduser', function ($iduser) {
+	User::verifyLogin();
+});
+
+
 
 $app->run();
