c3daa4e5f07ff95085e602a5866468df8c2cf8a1
Patrick Freitas <patrickdefreitas@Patricks-iMac.local>
2020-10-01 21:59:20 -0300

    Atualização do PHPMailer e correções
    Atualização PHPmailer, correção na classe Mailer.php, correção no composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'No version set (parsed as 1.0.0)',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'No version set (parsed as 1.0.0)',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.1.7',
+      'version' => '6.1.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2c2370ba3df7034f9eb7b8f387c97b52b2ba5ad0',
+    ),
+    'rain/raintpl' => 
+    array (
+      'pretty_version' => '3.1.1',
+      'version' => '3.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'edee683bf242f40cc75bee46a78759f6c1589dca',
+    ),
+    'slim/slim' => 
+    array (
+      'pretty_version' => '2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e95c9abbc57a8f023ab4dacebcab9dae4d9f1f0',
+    ),
+  ),
+);
