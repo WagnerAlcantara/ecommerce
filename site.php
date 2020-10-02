88c218f79d8da1067338db42eed2288af5ae7825
Patrick Freitas <patrickdefreitas@Patricks-iMac.local>
2020-10-01 21:51:53 -0300

    Atualização dependencia PHPmailer
    Atualização da dependencia PHPmailer

--- site.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use \Hcode\Page;
-
-/** Rota  default */
-$app->get('/', function () {
-  $page = new Page();
-  $page->setTpl("index");
-});
--- views-cache/categories-menu.704b5e3476d17cc99858f2034c2ed59b.rtpl.php
+++
@@ -1,1 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Android</a></li><li><a href="/categories/7">Apple</a></li><li><a href="/categories/4">Motorola</a></li><li><a href="/categories/6">Samsung</a></li>
